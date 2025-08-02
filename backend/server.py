cbb4e3f671e9c8092d3ac8da31672d5add2b7202
Ulises Lugo <uliseslf99@hotmail.com>
2025-08-02 15:11:20 -0600

    Add notification

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,8 @@
 import requests
 import time
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
 TICKETS_URL = 'https://www.realmadrid.com/es-ES/futbol/partidos/entradas/real-madrid-barcelona-26-10-2025'
 
@@ -11,16 +13,17 @@ KEY_LABELS = [
 ]
 
 class ContentGetter:
-    def __init__(self, url):
+    def __init__(self, requests, url):
+        self.requests = requests
         self.url = url
 
     def get_content(self):
         try:
-            response = requests.get(self.url)
+            response = self.requests.get(self.url)
             response.raise_for_status()  # Raise an error for bad responses
             response.encoding = 'utf-8' # Set encoding to UTF-8 for Spanish accents
             return response.text
-        except requests.RequestException as e:
+        except self.requests.RequestException as e:
             print(f"An error occurred while fetching the content: {e}")
             return None
 
@@ -39,9 +42,33 @@ class ContentChecker:
                 missing_labels.append(i)
         return missing_labels
     
+class NotificationSender:
+    def __init__(self, requests, api_key):
+        self.requests = requests
+        self.api_key = api_key
+
+    def send_notification(self):
+        notification_name = "ticketBot" 
+
+        url = f"https://api.pushcut.io/v1/notifications/{notification_name}"
+        headers = {
+            "API-Key": api_key
+        }
+        
+        response = requests.post(url, headers=headers)
+        print("NOTIFICATION Sent:", response.text)
+    
 if __name__ == "__main__":
-    content_getter = ContentGetter(TICKETS_URL)
+    load_dotenv()
     print("Ticketing server started")
+
+    api_key = os.getenv('API_KEY')
+    if api_key is None:
+        print("Error: API_KEY is not set in the environment variables.")
+        exit(1)
+
+    content_getter = ContentGetter(requests, TICKETS_URL)
+    notification_sender = NotificationSender(requests, api_key)
 
     while True:
         content = content_getter.get_content()
@@ -57,7 +84,7 @@ if __name__ == "__main__":
         if len(missing_labels) > 0:
             print("TICKETING CHANGED - Labels:", missing_labels)
 
-            # TODO: Implement the logic to send a notification or alert
+            notification_sender.send_notification()
             exit(1)
 
         print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] OK")
